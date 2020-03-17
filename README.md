ca8763ffdbfab2d847360b43bb5965b10a60e94d
zmuranaka <zmuranaka@gmail.com>
2020-03-17 10:53:09 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# YouTube URL Validator
+
+## What is this Project?
+
+This project is a web application that can check whether a YouTube URL is valid (it leads to a real video) or not. The user can enter the URL manually, or they can generate random URLs.
+
+## How do I use the Website?
+
+You can enter a URL manually in the text box labeled 'Validate by Direct Input'. Since all YouTube video URLs start with 'https://www.youtube.com/watch?v=', that is already provided for you. All you need to enter is the 11-character string that comes after that. If the URL is valid, it will appear under the 'Valid URLs' section. Otherwise, it will appear under the 'Invalid URLs' section. If you do not want to enter URLs manually, you can click the 'Start' button labeled 'Validate Randomly Generated URLs', and it will generate random YouTube URLs for you and automatically validate them. However, if you use this method, you are *extremely unlikely* to see it create a valid URL (see below for the explanation). You can click the 'Stop' button to stop it from creating random URLs. Finally, you can click the 'Clear' button to clear the 'Invalid URLs' and 'Valid URLs' sections.
+
+## Why is Creating URLs Randomly So Unlikely to Create a Valid URL?
+
+The explanation for this requires us understanding how a YouTube video's URL is constructed. Basically, every youtube video URL begins with 'https://www.youtube.com/watch?v=', and then has a string of 11 characters. Of these 11 characters, the first ten can be any of the following: any capital or lowercase letter, any digit, a single dash, or an underscore. In total, this is 64 different characters. Therefore, out of just these 10 characters, there are 64^10 different combinations, which is approximately 1.153 \* 10^18. However, there is also an 11th character, which can only be one of the following: '0', '4', '8', 'A', 'E', 'I', 'M', 'Q', 'U', 'Y', 'c', 'g', 'k', 'o', 's', or 'w' ([Source](http://conferences.sigcomm.org/imc/2011/docs/p371.pdf)). This gives us 16 more combinations, making the total number of combinations of the 11 characters 1.153 \* 10^18 \* 16, or approximately 1.845 \* 10^19. To put how large this number is in perspective, if you were to generate 1 billion random URLs, you would have generated only about 0.00000000005421 of the total URLs available.
+
+## Directories:
+
+images  
+&nbsp;&nbsp;&nbsp;&nbsp;This folder contains the social media logos.  
+scripts  
+&nbsp;&nbsp;&nbsp;&nbsp;This folder contains the JavaScript that creates and validates the YouTube URLs.  
+styles  
+&nbsp;&nbsp;&nbsp;&nbsp;This folder contains the stylesheet for the website.
+
+## Sources:
+
+I got the GitHub logo from [GitHub](https://github.com/logos) and I created the mail and link logos in Adobe Photoshop.  
+Websites that helped me create the project:  
+http://conferences.sigcomm.org/imc/2011/docs/p371.pdf  
+&nbsp;&nbsp;&nbsp;&nbsp;This research paper describes the correct format of a YouTube video URL. This was very helpful because many sources incorrectly believed that all 11 characters at the end of a YouTube URL are the same, when in reality the last character is unique.  
+https://gist.github.com/tonY1883/a3b85925081688de569b779b4657439b  
+&nbsp;&nbsp;&nbsp;&nbsp;This trick from GitHub user tonY1883 gave me the idea of how to validate a YouTube URL with plain JavaScript.  
+https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp  
+&nbsp;&nbsp;&nbsp;&nbsp;W3Schools was very helpful as a general reference.
+
+## Author:
+
+Zachary Muranaka  
+&nbsp;&nbsp;&nbsp;&nbsp;zacharymuranaka@mail.weber.edu  
+&nbsp;&nbsp;&nbsp;&nbsp;http://icarus.cs.weber.edu/~zm83483/
---
+++ scripts/validator.js
@@ -0,0 +1,175 @@
+"use strict";
+
+/*
+File: validator.js
+Zachary Muranaka
+Allows the user to validate YouTube URLs, either by direct input or randomly
+*/
+
+var linkValidity = false;
+var invalidCount = 0;
+var validCount = 0;
+var startHasBeenClicked = false;
+var stopHasNotBeenClicked = true;
+
+document.getElementById("validateBtn").addEventListener("click",
+function()
+{
+    checkLinkValidity(document.getElementById("validateTxtBox").value);
+});
+
+document.getElementById("startBtn").addEventListener("click",
+function()
+{
+    startHasBeenClicked = true; // The start button was clicked
+    stopHasNotBeenClicked = true; // The stop button has not been clicked
+    getRandomLink();
+});
+
+// Add an event listener to the page so pressing enter calls the getRandomLink() function
+window.addEventListener("keyup",
+function(e)
+{
+    if(e.keyCode == 13) // 13 is the keycode for enter
+    {
+        startHasBeenClicked = true; // Pressing enter is equivalent to clicking start
+        stopHasNotBeenClicked = true; // The stop button has not been clicked
+        getRandomLink();
+    }
+});
+
+document.getElementById("stopBtn").addEventListener("click",
+function()
+{
+    startHasBeenClicked = false; // The start button was not clicked
+    stopHasNotBeenClicked = false; // The stop button has been clicked
+});
+
+document.getElementById("clearBtn").addEventListener("click",
+function()
+{
+    let invalidLinks = document.getElementsByClassName("invalidLink");
+    let validLinks = document.getElementsByClassName("validLink");
+
+    // Destroy all invalid links
+    while(invalidLinks.length > 0)
+    {
+        invalidLinks[0].parentNode.removeChild(invalidLinks[0]); // Destroy the first invalidLink
+        invalidCount--;
+    }
+
+    // Destroy all valid links
+    while(validLinks.length > 0)
+    {
+        validLinks[0].parentNode.removeChild(validLinks[0]); // Destroy the first validLink
+        validCount--;
+    }
+
+    // Reset the invalidCounter and validCounter
+    document.getElementById("invalidCounter").textContent = invalidCount;
+    document.getElementById("validCounter").textContent = validCount;
+});
+
+/*
+ * A YouTube video's web address is https://www.youtube.com/watch?v=xxxxxxxxxxy
+ * Where x can be a single character of any of the following:
+ * A single dash, 0 to 9, A to Z, an underscore, a to z
+ * And y, the last character, can only be one of the following:
+ * 0, 4, 8, A, E, I, M, Q, U, Y, c, g, k, o, s, w
+ *
+ * Reference: http://conferences.sigcomm.org/imc/2011/docs/p371.pdf
+ */
+function getRandomLink()
+{   
+    let randomChars = "";
+
+    // Fill randomChars with 10 random characters that can be in a YouTube URL
+    for(let i = 0; i < 10; i++)
+        randomChars += String.fromCharCode(generateAsciiCode()); // Convert from ASCII code to char
+
+    randomChars += generateLastChar(); // End randomChars with one of the valid last chars
+
+    checkLinkValidity(randomChars);
+}
+
+// Returns an ASCII code for one of the following: a single dash, 0 to 9, A to Z, an underscore, or a to z
+function generateAsciiCode()
+{
+    let randomNum = Math.floor(Math.random() * 64) + 1; // Generates a random number 1 to 64
+
+    if(randomNum == 1)
+        return 45; // ASCII code for single dash
+    else if(randomNum >= 2 && randomNum <= 11)
+        return randomNum + 46; // ASCII codes for 0 to 9
+    else if(randomNum >= 12 && randomNum <= 37)
+        return randomNum + 53; // ASCII codes for A to Z
+    else if(randomNum == 38)
+        return 95; // ASCII code for underscore
+    else
+        return randomNum + 58; // ASCII code for a to z
+}
+
+// Returns a valid last character of a YouTube URL
+function generateLastChar()
+{
+    const lastCharArray = ['0', '4', '8', 'A', 'E', 'I', 'M', 'Q', 'U', 'Y', 'c', 'g', 'k', 'o', 's', 'w'];
+    return lastCharArray[Math.floor(Math.random() * 16)]; // Returns a random member of the lastCharArray
+}
+
+// Checks if a link is a valid YouTube video URL
+function checkLinkValidity(endOfLink)
+{
+    let startOfLink = "https://www.youtube.com/watch?v="; // Initialize the start of the link
+
+    var thumbnail = getThumbnail(endOfLink);
+    thumbnail.onload =
+    function()
+    {
+        /*
+         * A valid YouTube URL will have a thumbnail of width 320px and height 180px
+         * An invalid YouTube URL will be given a default thumbnail of width 120px and height 90px
+         * Therefore, we check the validity of the link based on the thumbnail YouTube gives it
+         */
+        linkValidity = this.width === 320 && this.height === 180;
+        createNewP(startOfLink, endOfLink);
+    };
+}
+
+// Returns an Image object with the src of the YouTube video's thumbnail
+function getThumbnail(characters)
+{
+    // Create an img with the src of the YouTube URL's thumbnail
+    var thumbnail = new Image();
+    thumbnail.src = "https://img.youtube.com/vi/" + characters + "/mqdefault.jpg";
+
+    return thumbnail;
+}
+
+// Creates a new paragraph element that is appended to the document
+function createNewP(startOfLink, endOfLink)
+{
+    if(!linkValidity)
+    {
+        invalidCount++; // We have found another invalid link
+        var newInvalidLink = document.createElement("p");
+        newInvalidLink.innerHTML = startOfLink + endOfLink;
+        newInvalidLink.classList.add("invalidLink"); // Give this element the class of "invalidLink"
+        newInvalidLink.style.float = "left";
+        document.getElementById("invalidLinks").appendChild(newInvalidLink); // Add this p to the document
+        document.getElementById("invalidCounter").textContent = invalidCount; // Update the invalidCounter
+        if(startHasBeenClicked && stopHasNotBeenClicked)
+            getRandomLink();
+    }
+    else
+    {
+        validCount++; // We found a valid link
+        var newValidLink = document.createElement("p");
+        newValidLink.innerHTML = startOfLink + endOfLink;
+        newValidLink.classList.add("validLink"); // Give this element the class of "validLink"
+        newValidLink.style.float = "left";
+        document.getElementById("validLinks").appendChild(newValidLink); // Add this p to the document
+        document.getElementById("validCounter").textContent = validCount; // Update the validCounter
+        if(startHasBeenClicked && stopHasNotBeenClicked)
+            getRandomLink();
+    }
+}
